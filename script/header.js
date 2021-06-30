a5bad03637f8264790b973ad551c320b4d6b7e81
Gabriela Fogaça <gabifogaca.gabriela@gmail.com>
2021-06-30 10:28:43 -0300

    arrumar navbar tela pequena

--- script/header.js
+++ script/header.js
@@ -4,9 +4,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault()
+    if(event.type ==='click') event.preventDefault()
     const nav = document.querySelector('.navbar-links')
     nav.classList.toggle('active')
 }
 
-
+menuResponsive.addEventListener('click', toggleMenu)
 menuResponsive.addEventListener('touchstart', toggleMenu)
