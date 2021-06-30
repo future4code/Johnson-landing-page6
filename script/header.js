62c1614f8be2dd08f7370384b01225dbcc5a56ab
Gabriela Fogaça <gabifogaca.gabriela@gmail.com>
2021-06-29 21:43:02 -0300

    NAVBAR - GABRIELA

--- script/header.js
+++ script/header.js
@@ -1,6 +1,12 @@
 const menuResponsive = document.getElementsByClassName('menu-responsive')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-menuResponsive.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault()
+    const nav = document.querySelector('.navbar-links')
+    nav.classList.toggle('active')
+}
+
+
+menuResponsive.addEventListener('touchstart', toggleMenu)
